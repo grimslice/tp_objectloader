a7ce563ae9f9b631b3ca640315bca764f9270ac0
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2026-02-24 15:51:36 +0200

    Create ranch1.lua

---
+++ tp_objectloader/data/ranch1.lua
@@ -0,0 +1,697 @@
+---------------------------------------------------------------
+--[[ Functions ]]--
+---------------------------------------------------------------
+
+-- (!) DO NOT RENAME THE `data` variable, it must always be as `data`.
+-- Checkout the examples below how we create objects to the specified locations.
+
+local data = { 
+
+    -- chickens
+
+    {
+        Object = 'stall3',
+
+        Coords = { 
+            x = -835.8909301757812, y = 368.5548400878906, z = 97.12201690673828,
+            pitch = 0, roll = 0, yaw = -132.8577117919922
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_chickencoopcart01x',
+    
+        Coords = { 
+            x = -847.3221435546875, y = 370.23431396484375, z = 96.27522277832031,
+            pitch = 0, roll = 0, yaw = -144.73056030273438 
+        },
+    
+        PlaceObjectOnGroundProperly = true,
+    
+        ObjectRenderDistance = 30,
+    },
+
+    { 
+        Object = 'p_chickencoopcart01x',
+    
+        Coords = { 
+            x = -843.259033203125, y = 369.97509765625, z = 96.18472290039062,
+            pitch = 0, roll = 0, yaw = 136.27561950683594
+        },
+    
+        PlaceObjectOnGroundProperly = true,
+    
+        ObjectRenderDistance = 30,
+    },
+
+    { 
+        Object = 'p_cratechicken01x',
+    
+        Coords = { 
+            x = -844.4816284179688, y = 370.9845886230469, z = 96.09825134277344,
+            pitch = -4.81872272491455, roll = 1.28703761100769, yaw = -30.27440452575683
+        },
+    
+        PlaceObjectOnGroundProperly = true,
+    
+        ObjectRenderDistance = 30,
+    },
+
+    { 
+        Object = 'p_cratechicken01x',
+    
+        Coords = { 
+            x = -847.9949340820312, y = 367.59027099609375, z = 96.35626983642578,
+            pitch = 5.6051230430603, roll = 3.14572668075561, yaw = 112.14987182617188
+        },
+    
+        PlaceObjectOnGroundProperly = true,
+    
+        ObjectRenderDistance = 30,
+    },
+
+    { 
+        Object = 'p_cratechicken01x',
+    
+        Coords = { 
+            x = -846.0127563476562, y = 370.8735656738281, z = 96.19662475585938,
+            pitch = -2.72945475578308, roll = 3.25500178337097, yaw = 5.25328874588012
+        },
+    
+        PlaceObjectOnGroundProperly = true,
+    
+        ObjectRenderDistance = 30,
+    },
+
+    
+    { 
+        Object = 'p_haypile01x',
+    
+        Coords = { 
+            x = -847.217041015625, y = 369.0361328125, z = 96.2722396850586,
+            pitch = 0, roll = 0, yaw = 58.60747146606445
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 30,
+    },
+
+    { 
+        Object = 'p_haypile01x',
+    
+        Coords = { 
+            x = -846.4501953125, y = 368.3997497558594, z = 96.21643829345703,
+            pitch = -3.66833186149597, roll = 2.37993383407592, yaw = -29.6493911743164
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 30,
+    },
+
+    { 
+        Object = 'p_haypile01x',
+    
+        Coords = { 
+            x = -845.5811157226562, y = 368.6007385253906, z = 96.21643829345703,
+            pitch = 0, roll = 0, yaw = -5.51836585998535
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 30,
+    },
+    
+    { 
+        Object = 'p_haypile01x',
+    
+        Coords = { 
+            x = -843.8093872070312, y = 368.4843444824219, z = 96.18768310546875,
+            pitch = 0, roll = 0, yaw = -115.18746185302734
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 30,
+    },
+
+    { 
+        Object = 'p_feedbags02x',
+    
+        Coords = { 
+            x = -848.2047729492188, y = 366.3968811035156, z = 96.06497192382812,
+            pitch = -5.03995513916015, roll = 7.19348335266113, yaw = 154.23838806152344
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 50,
+    },
+
+    { 
+        Object = 'p_feedbag01bx',
+    
+        Coords = { 
+            x = -847.6836547851562, y = 365.9695739746094, z = 96.27824401855469,
+            pitch = 0, roll = 0, yaw = 11.4263334274292
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 50,
+    },
+    
+    { 
+        Object = 'p_bucket03x',
+    
+        Coords = { 
+            x = -846.7791748046875, y = 366.4891357421875, z = 96.30794525146484,
+            pitch = 0, roll = 0, yaw = 0.21410399675369
+        },
+    
+        PlaceObjectOnGroundProperly = true,
+    
+        ObjectRenderDistance = 50,
+    },
+
+    { 
+        Object = 'p_watertroughsml01x',
+    
+        Coords = { 
+            x = -843.3790893554688, y = 366.311767578125, z = 96.22407531738281,
+            pitch = 0, roll = 0, yaw = -1.67571067810058
+        },
+    
+        PlaceObjectOnGroundProperly = true,
+    
+        ObjectRenderDistance = 50,
+    },
+
+
+
+    -- cow
+
+    { 
+        Object = 'stall6',
+    
+        Coords = { 
+            x = -848.5371704101562, y = 359.1956481933594, z = 98.18057250976562,
+            pitch = 1.62883973121643, roll = -3.50995588302612, yaw = 26.1608943939209
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_haybalestack01x',
+    
+        Coords = { 
+            x = -842.6146850585938, y = 358.3959045410156, z = 96.02857208251953,
+            pitch = -2.77314901351928, roll = -3.85654497146606, yaw = 166.8583221435547
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_haybalecover01x',
+    
+        Coords = { 
+            x = -843.786865234375, y = 356.7479248046875, z = 96.08927154541016,
+            pitch = 2.77594995498657, roll = -2.58546924591064, yaw = 15.4200325012207
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+
+
+    { 
+        Object = 'p_watertroughsml01x',
+    
+        Coords = { 
+            x = -851.8396606445312, y = 358.9067687988281, z = 96.256591796875,
+            pitch = 0, roll = 0, yaw = 78.72692108154297
+        },
+    
+        PlaceObjectOnGroundProperly = true,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_watertroughsml01x',
+    
+        Coords = { 
+            x = -845.664306640625, y = 357.3084411621094, z = 96.26383209228516,
+            pitch = 1.00000715255737, roll = 1.99997639656066, yaw = -103.2339096069336
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+
+    { 
+        Object = 'p_fence02bx',
+    
+        Coords = { 
+            x = -850.9639892578125, y = 359.9944763183594, z = 96.29576110839844,
+            pitch = 0, roll = 0, yaw = 172.69171142578125
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_fence02bx',
+    
+        Coords = { 
+            x = -848.6715087890625, y = 357.9427795410156, z = 96.27449035644531,
+            pitch = -1.82579231262207, roll = -3.41174626350402, yaw = 79.14602661132812
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+    
+    { 
+        Object = 'p_fence02bx',
+    
+        Coords = { 
+            x = -845.9824829101562, y = 358.7353820800781, z = 96.38563537597656,
+            pitch = -3.6724841594696, roll = 0.11929063498973, yaw = 167.30235290527344
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_lantern04x',
+    
+        Coords = { 
+            x = -842.8795776367188, y = 367.2423095703125, z = 97.19376373291016,
+            pitch = 0, roll = 0, yaw = -117.7484359741211
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_barrel02x',
+    
+        Coords = { 
+            x = -842.8397827148438, y = 367.2144775390625, z = 96.2055892944336,
+            pitch = 0, roll = 0, yaw = -124.61939239501953
+        },
+    
+        PlaceObjectOnGroundProperly = true,
+    
+        ObjectRenderDistance = 100,
+    },
+    
+    { 
+        Object = 'p_barrel02x',
+    
+        Coords = { 
+            x = -847.9412841796875, y = 361.7242126464844, z = 96.24972534179688,
+            pitch = 0, roll = 0, yaw = -158.25929260253906
+        },
+    
+        PlaceObjectOnGroundProperly = true,
+    
+        ObjectRenderDistance = 100,
+    },
+    
+    { 
+        Object = 'p_lantern04x',
+    
+        Coords = { 
+            x = -847.9541625976562, y = 361.7206115722656, z = 97.32310485839844,
+            pitch = 0, roll = 0.00000834459387, yaw = 169.55979919433594
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+
+    -- delivery for milk coords
+
+    { 
+        Object = 'p_ambcart02x',
+    
+        Coords = { 
+            x = -831.5654296875, y = 357.0947265625, z = 95.87287139892578,
+            pitch = 44.27646255493164, roll = 12.83920097351074, yaw = -100.70472717285156
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+    
+    { 
+        Object = 'p_cart01x',
+    
+        Coords = { 
+            x = -829.6148681640625, y = 356.4709167480469, z = 95.7162857055664,
+            pitch = -5.559907913208, roll = -1.63436114788055, yaw = 120.77626037597656
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+    
+    { 
+        Object = 'p_haybale01x',
+    
+        Coords = { 
+            x = -829.7554321289062, y = 356.8268737792969, z = 96.54644775390625,
+            pitch = 0, roll = 0, yaw = -76.38800811767578
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_haybale01x',
+    
+        Coords = { 
+            x = -829.9025268554688, y = 355.8115234375, z = 96.38639831542969,
+            pitch = 0, roll = 0, yaw = -58.07286071777344
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+    
+    { 
+        Object = 'p_milkcan01x',
+    
+        Coords = { 
+            x = -832.2593383789062, y = 356.0368347167969, z = 95.66634368896484,
+            pitch = 0, roll = 0, yaw = -95.8512191772461
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_milkcan01x',
+    
+        Coords = { 
+            x = -832.11474609375, y = 355.5096435546875, z = 95.66634368896484,
+            pitch = 0, roll = 0, yaw = -76.69694519042969
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_milkcan01x',
+    
+        Coords = { 
+            x = -832.4413452148438, y = 355.00018310546875, z = 95.66634368896484,
+            pitch = 0, roll = 0, yaw = -90.0508041381836
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_milkcan01x',
+    
+        Coords = { 
+            x = -831.287109375, y = 358.3716735839844, z = 95.68536376953125,
+            pitch = 7.40738773345947, roll = 2.35863161087036, yaw = -78.64884185791016
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    -- feed chickens
+
+    { 
+        Object = 'p_feedbags01x',
+    
+        Coords = { 
+            x = -864.7347412109375, y = 327.9211120605469, z = 95.28400421142578,
+            pitch = 0, roll = 0, yaw = 170.75860595703125 
+        },
+    
+        PlaceObjectOnGroundProperly = true,
+    
+        ObjectRenderDistance = 50,
+    },
+
+    
+    { 
+        Object = 'p_feedbags02x',
+    
+        Coords = { 
+            x = -865.5957641601562, y = 329.4508361816406, z = 94.80551147460938,
+            pitch = 0, roll = 0, yaw = 24.01669692993164
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 50,
+    },
+
+    -- sheeps
+
+
+    { 
+        Object = 'p_fence11x',
+    
+        Coords = { 
+            x = -857.826416015625, y = 359.1481018066406, z = 96.10449981689453,
+            pitch = 0.00000576299134, roll = -0.56187409162521, yaw = 81.33283233642578
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_fence11x',
+    
+        Coords = { 
+            x = -857.5712280273438, y = 360.85498046875, z = 96.1168441772461,
+            pitch = 0, roll = 0, yaw = 81.17335510253906
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_fence11x',
+    
+        Coords = { 
+            x = -857.2562866210938, y = 362.5263366699219, z = 96.09115600585938,
+            pitch = -4.13593864440918, roll = 1.25458204746246, yaw = 81.2041244506836
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_fence11x',
+    
+        Coords = { 
+            x = -856.9805297851562, y = 364.2568664550781, z = 96.12478637695312,
+            pitch = -4.23369741439819, roll = 0.36586606502532, yaw = 80.81177520751953
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_fence11x',
+    
+        Coords = { 
+            x = -856.6826171875, y = 365.96771240234375, z = 96.11247253417969,
+            pitch = -4.49724292755126, roll = 0.79994255304336, yaw = 81.2057113647461
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_fence11x',
+    
+        Coords = { 
+            x = -856.4564819335938, y = 367.7774353027344, z = 96.05318450927734,
+            pitch = -1.11925911903381, roll = -4.25799798965454, yaw = -10.9192533493042
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    
+    { 
+        Object = 'p_fence11x',
+    
+        Coords = { 
+            x = -854.7489624023438, y = 367.4435119628906, z = 96.1899642944336,
+            pitch = -2.01557517051696, roll = -4.00211858749389, yaw = -14.1312141418457
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_fence11x',
+    
+        Coords = { 
+            x = -853.02978515625, y = 367.12200927734375, z = 96.32245635986328,
+            pitch = 0, roll = 0, yaw = -55.30340194702148
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    { 
+        Object = 'p_fence11x',
+    
+        Coords = { 
+            x = -852.0338745117188, y = 365.67022705078125, z = 96.31555938720703,
+            pitch = 0, roll = 0, yaw = -63.88584899902344
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+
+    { 
+        Object = 'p_fence02_post',
+    
+        Coords = { 
+            x = -851.6555786132812, y = 362.3197021484375, z = 96.24806213378906,
+            pitch = 0, roll = 0, yaw = 77.1261215209961
+        },
+    
+        PlaceObjectOnGroundProperly = false,
+    
+        ObjectRenderDistance = 100,
+    },
+
+    -- removing existing objects
+    { 
+        Object = 'p_fence11x',
+    
+        Coords = { 
+            x = -851.69970703125, y = 362.3265686035156, z = 96.43798828125
+        },
+    
+
+        ObjectRenderDistance = 50,
+        RemoveExistingObject = true,
+    },
+
+    { 
+        Object = 'p_watertrough02x',
+    
+        Coords = { 
+            x = -853.7581176757812, y = 359.4405517578125, z = 96.21808624267578,
+            pitch = 0, roll = 0, yaw = 176.81683349609375,
+        },
+    
+
+        ObjectRenderDistance = 50,
+    },
+
+    { 
+        Object = 'p_watertrough02x',
+    
+        Coords = { 
+            x = -856.3640747070312, y = 359.6268310546875, z = 96.16529083251953,
+            pitch = 0, roll = 0, yaw = 176.57376098632812,
+        },
+    
+
+        ObjectRenderDistance = 50,
+    },
+
+    { 
+        Object = 'p_watertrough02x',
+    
+        Coords = { 
+            x = -855.0785522460938, y = 366.94061279296875, z = 96.22856903076172,
+            pitch = -1.51177501678466, roll = -4.85141801834106, yaw = -11.73083591461181,
+        },
+    
+
+        ObjectRenderDistance = 50,
+    },
+
+    { 
+        Object = 'p_feedtrough01x',
+    
+        Coords = { 
+            x = -852.2225341796875, y = 361.6144104003906, z = 96.36921691894531,
+            pitch = 1.30525469779968, roll = 4.12099361419677, yaw = -102.03814697265625,
+        },
+    
+
+        ObjectRenderDistance = 50,
+    },
+
+}
+
+
+---------------------------------------------------------------
+--[[ DO NOT TOUCH ]]--
+---------------------------------------------------------------
+
+Citizen.CreateThread(function() for _, v in pairs (data) do InsertData(v) end end)
